6de7191d8981402538a5da28e840ccdfee5a9a9f
sanasdh <sanasdh@gmail.com>
2020-10-19 17:07:08 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component{
+  constructor(){
+    super();
+    this.state={
+      value:null
+    }
+
+  }
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header"> 
+     <h1> My React Calculator </h1>
       </header>
+      <div className="outerBox">
+        <div className="outPut">
+
+        </div>
+
+        <div className="input">
+          <div className="numbers">
+            <div className="number" id="7">7</div>
+            <div className="number" id="8">8</div>
+            <div className="number" id="9">9</div>
+            <div className="number" id="4">4</div>
+            <div className="number" id="5">5</div>
+            <div className="number" id="6">6</div>
+            <div className="number" id="1">1</div>
+            <div className="number" id="2">2</div>
+            <div className="number" id="3">3</div>
+            <div className="number" id="0">0</div>
+            <div className="number" id="00">00</div>
+            <div className="number" id="dot">.</div>
+
+          </div>
+        </div>
+      </div>
     </div>
   );
+}
 }
 
 export default App;
