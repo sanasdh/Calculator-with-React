4d0a8d6eb95c3431303debf798e4a23feb0822ea
sanasdh <sanasdh@gmail.com>
2020-10-20 16:23:09 -0400

    handleclick

--- src/App.js
+++ src/App.js
@@ -6,10 +6,21 @@ class App extends Component{
   constructor(){
     super();
     this.state={
-      value:null,
-      result: 0
+      value:[],
+      value1:[],
+      result: 0,
+      readout:0,
     }
-
+  }
+  handleClick=(e)=>{
+    e.preventDefault()
+if(e.target.className ==="number"){
+  let val= e.target.innerHTML
+  this.setState({
+    value1:[...this.state.value1, val],
+    readout: [...this.state.value1, val],
+  })
+}
   }
   render(){
   return (
@@ -19,22 +30,22 @@ class App extends Component{
       </header>
       <div className="outerBox">
         <div className="outPut">
-{this.state.result}
+{this.state.readout}
         </div>
 
         <div className="input">
           <div className="numbers">
-            <div className="number" id="seven">7</div>
-            <div className="number" id="8">8</div>
-            <div className="number" id="9">9</div>
-            <div className="number" id="4">4</div>
-            <div className="number" id="5">5</div>
-            <div className="number" id="6">6</div>
-            <div className="number" id="1">1</div>
-            <div className="number" id="2">2</div>
-            <div className="number" id="3">3</div>
-            <div className="number" id="0">0</div>
-            <div className="number" id="00">00</div>
+            <div className="number" id="seven" onClick={this.handleClick}>7</div>
+            <div className="number" id="8" onClick={this.handleClick}>8</div>
+            <div className="number" id="9" onClick={this.handleClick}>9</div>
+            <div className="number" id="4" onClick={this.handleClick}>4</div>
+            <div className="number" id="5" onClick={this.handleClick}>5</div>
+            <div className="number" id="6" onClick={this.handleClick}>6</div>
+            <div className="number" id="1" onClick={this.handleClick}>1</div>
+            <div className="number" id="2" onClick={this.handleClick}>2</div>
+            <div className="number" id="3" onClick={this.handleClick}>3</div>
+            <div className="number" id="0" onClick={this.handleClick}>0</div>
+            <div className="number" id="00" onClick={this.handleClick}>00</div>
             <div className="number" id="dot">.</div>
           </div>
 
