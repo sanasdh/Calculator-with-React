9c8333569f814878b76e6ae27c539104c3d49d00
sanasdh <sanasdh@gmail.com>
2020-10-22 12:22:39 -0400

    Updates

---
+++ precache-manifest.046a38cafbf7dea77ef7e063e3880a0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ede475d359ff8f4d193df6a05739d85",
+    "url": "/Calculator-with-React/index.html"
+  },
+  {
+    "revision": "554007f716e3d60ed7ad",
+    "url": "/Calculator-with-React/static/css/main.d89ee4a8.chunk.css"
+  },
+  {
+    "revision": "01576db6c59b4db6788e",
+    "url": "/Calculator-with-React/static/js/2.c27c4a5b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Calculator-with-React/static/js/2.c27c4a5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "554007f716e3d60ed7ad",
+    "url": "/Calculator-with-React/static/js/main.a7dd5281.chunk.js"
+  },
+  {
+    "revision": "9bc472da68794d4f4867",
+    "url": "/Calculator-with-React/static/js/runtime-main.7e71df25.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Calculator-with-React/static/media/logo.5d5d9eef.svg"
+  }
+]);
